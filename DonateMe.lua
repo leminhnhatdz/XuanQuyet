fc6a517ff005a56808cfc0175f4c6bfc612d8e17
𝕏𝕦𝕒𝕟 ℚ𝕦𝕪𝕖𝕥 <166893675+leminhnhatdz@users.noreply.github.com>
2024-05-06 17:42:05 +0700

    Update DonateMe.lua

--- DonateMe.lua
+++ DonateMe.lua
@@ -3,7 +3,7 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 ----------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
-    Title = "Wibu Hub",
+    Title = "XuanQuyet Hub",
     SubTitle = "Skid Hub Beta 99 | By Le Minh Nhat",
     TabWidth = 160,
     Size = UDim2.fromOffset(500, 320),
